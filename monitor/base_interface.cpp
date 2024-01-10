a06e995590a9f97f8cba47bd839fd6955d998d79
AnastasiaSudneva <156110144+AnastasiaSudneva@users.noreply.github.com>
2024-01-10 18:01:21 +0500

    Update base_interface.cpp

--- monitor/base_interface.cpp
+++ monitor/base_interface.cpp
@@ -11,14 +11,150 @@ namespace fs = std::filesystem;
 
 static bool wdt_create_pipe([[maybe_unused]] int fd[2])
 {
-    // @TODO - создать pipe
+     if (pipe(fd) < 0)
+    {
+        std::cerr << "Failed to create pipe" << std::endl;
+        return false;
+    }
+
+    if (pid == -1)
+    {
+        std::cerr << "error when forked, errno: " << errno << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (pid == 0)
+    {
+        const std::string str = "Hello, world!";
+        std::cout << "I'm a child process" << std::endl;
+        write(fd[1], str.c_str(), str.size());
+        std::cout << "Write, str = " << str << std::endl;
+    }
+    else if (waitpid(pid, nullptr, 0))
+    {
+        char buf[64];
+        std::cout << "I'm a parent process" << std::endl;
+        if (read(fd[0], buf, sizeof(buf)) > 0)
+        {
+            std::cout << "Read, buf = " << buf << std::endl;
+        }
+    }
+    int n = fcntl(pipedes[0], F_GETFL);
+    fcntl(pipedes[0], F_SETFL, n | O_NONBLOCK);
+    n = fcntl(pipedes[1], F_GETFL);
+    fcntl(pipedes[1], F_SETFL, n | O_NONBLOCK);
+
     return true;
 }
 
 static pid_t run_program([[maybe_unused]] fs::path path, [[maybe_unused]] const std::vector<std::string>& args)
 {
-    // @TODO - написать запуск программы
-    return -1;
+    std::vector<const char*> argv;
+    argv.push_back(path.c_str());
+    for (const std::string& arg : args)
+    {
+        argv.push_back(arg.c_str());
+    }
+    argv.push_back(NULL);
+
+    posix_spawn_file_actions_t fileActions;
+
+    int result = posix_spawn_file_actions_init(&fileActions);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawn_file_actions_init failed" << std::endl;
+        return -1;
+    }
+
+    result = posix_spawn_file_actions_addclose(&fileActions, STDOUT_FILENO);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawn_file_actions_addclose failed" << std::endl;
+        return -1;
+    }
+
+    posix_spawn_file_actions_t* pFileActions = &fileActions;
+
+    posix_spawnattr_t attr;
+
+    result = posix_spawnattr_init(&attr);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawnattr_init failed" << std::endl;
+        return -1;
+
+        result = posix_spawn_file_actions_addclose(&fileActions, STDOUT_FILENO);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawn_file_actions_addclose failed" << std::endl;
+        return -1;
+    }
+
+    posix_spawn_file_actions_t* pFileActions = &fileActions;
+
+    posix_spawnattr_t attr;
+
+    result = posix_spawnattr_init(&attr);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawnattr_init failed" << std::endl;
+        return -1;
+    }
+
+    result = posix_spawnattr_setflags(&attr, POSIX_SPAWN_SETSIGMASK);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawnattr_setflags failed" << std::endl;
+        return -1;
+    }
+
+    sigset_t mask;
+    sigfillset(&mask);
+    result = posix_spawnattr_setsigmask(&attr, &mask);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawnattr_setsigmask failed" << std::endl;
+        return -1;
+    }
+    posix_spawnattr_t* pAttr = &attr;
+
+    pid_t childPid;
+    result = posix_spawnp(&childPid, path.c_str(), pFileActions, pAttr, const_cast<char**>(&argv[0]),
+        nullptr);
+    if (result != 0)
+    {
+        std::cerr << "posix_spawn failed" << std::endl;
+        return -1;
+    }
+
+    if (pAttr != NULL)
+    {
+        result = posix_spawnattr_destroy(pAttr);
+        if (result != 0)
+        {
+            std::cerr << "posix_spawnattr_destroy failed" << std::endl;
+            return -1;
+        }
+    }
+
+    if (pFileActions != NULL) 
+    {
+        result = posix_spawn_file_actions_destroy(pFileActions);
+        if (result != 0)
+        {
+            std::cerr << "posix_spawn_file_actions_destroy failed" << std::endl;
+            return -1;
+        }
+    }
+
+    int status;
+    result = waitpid(childPid, &status, WNOHANG);
+    if (result == -1)
+    {
+        std::cerr << "waitpid failed" << std::endl;
+        return -1;
+    }
+    return childPid;
 }
 
 static pid_t run_program(fs::path path)
@@ -87,8 +223,22 @@ bool IBaseInterface::PreparePrograms()
     m_progs.clear();
     for ([[maybe_unused]] auto& it : predefined_progs)
     {
-        // @TODO - добавить инициализацию программ в m_progs, в том числе аргумент командной строки
-        // здесь по сути просто переложить из одной структуры в другую
+        if (argc > 1)
+            program_argument += " " + std::string(argv[1]);
+
+        // Инициализируем программу и добавляем ее в m_progs
+        t_prog prog;
+        prog.pid = 0;
+        prog.path = it.path;
+        foreach(const char* const& arg, it.args)
+        {
+            if (arg)
+            {
+                prog.args.push_back(arg);
+            }
+        }
+        prog.watched = it.watched;
+        m_progs.push_back(prog);
     }
 
     return true;
@@ -96,7 +246,9 @@ bool IBaseInterface::PreparePrograms()
 
 bool IBaseInterface::TerminateProgram([[maybe_unused]] const pid_t pid) const
 {
-    // @TODO - написать терминирование процесса по заданному pid
+    if (kill(pid, SIGKILL) == 0) {
+        return true;
+    }
     return false;
 }
 
@@ -120,8 +272,7 @@ pid_t IBaseInterface::FindTerminatedTask() const
 
 bool IBaseInterface::GetRequestTask([[maybe_unused]] pid_t& pid) const
 {
-    // @TODO - считать пид процесса, который пинговал из пайпа
-    return true;
+    return sizeof(pid_t) == read(m_wdtPipe[0], &pid, sizeof(pid_t));
 }
 
 bool IBaseInterface::WaitExitAllPrograms() const
@@ -137,13 +288,38 @@ bool IBaseInterface::WaitExitAllPrograms() const
 
 bool IBaseInterface::ToDaemon() const
 {
-    // @TODO - демонизировать процесс мониторинга
+     pid_t pid = fork();
+
+    if (pid < 0) {
+        std::cerr << "Ошибка при создании процесса" << std::endl;
+        return false;
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    if (setsid() < 0)
+    {
+        std::cerr << "Ошибка при создании нового SID" << std::endl;
+        return false;
+    }
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
     return true;
 }
 
 void IBaseInterface::Destroy()
 {
-    // @TODO - закрыть файловые дескрипторы пайпа
+     if (m_wdtPipe[0] != -1)
+    {
+        close(m_wdtPipe[0]);
+    }
+    if (m_wdtPipe[1] != -1)
+    {
+        close(m_wdtPipe[1]);
+    }
     m_init = false;
 }
 
